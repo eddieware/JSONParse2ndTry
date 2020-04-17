1572f3ac043b1b5a521ccd33a1c9985fed7f086b
Eduardo Mejia Bautista <ing.eduardo.mejia.bautista@gmail.com>
2020-04-17 15:43:53 -0500

    updateOKParse

--- JSONParse2ndTry/ContentView.swift
+++ JSONParse2ndTry/ContentView.swift
@@ -9,8 +9,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    // 1.
+    @ObservedObject var fetch = FetchToDo()
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            
+            // 2.
+            // fetch ObservedObject . todos @Published var todos
+            List(fetch.todos) { todo in
+                VStack(alignment: .leading) {
+                    // 3.
+                    //Text(todo.userId.description)
+                    Text(todo.id.description) //description es usada como un to string porque es un INT
+                    Text(todo.title) //accedemos a la propiedad title del modelo
+                        Text(todo.body)
+                            
+                    
+                        .font(.system(size: 11))
+                        .foregroundColor(Color.gray) //description es usada como un to string porque es un INT
+                    
+                    Text("Press the button")
+                    
+                       Button(action: {
+                       print("pressed!!!!!!!!")
+                       
+                       }) {Text("Clickme")}
+                    
+                    
+                }
+            }
+        }
     }
 }
 
---
+++ JSONParse2ndTry/Model.swift
@@ -0,0 +1,53 @@
+//
+//  Model.swift
+//  JSONParse2ndTry
+//
+//  Created by EDUARDO MEJIA on 31/03/20.
+//  Copyright © 2020 EDDIEWARE. All rights reserved.
+//
+
+
+import Foundation
+import SwiftUI
+
+
+
+struct Todo: Codable, Identifiable {
+    public var userId: Int
+    public var id: Int
+    public var title: String
+    public var body: String
+}
+
+//CODEAABLE E IDENTIFIABLE IS A PROTOCOL
+
+//The struct conforms to the Codable protocol to be able to decode the model from the JSON File and the struct conforms to the Identifiable protocol, which allows the items to be listed in a List.
+class FetchToDo: ObservableObject {
+  // 1.
+
+
+    
+    
+  @Published var todos = [Todo]() //Encierras el modelo en una variable @Published properti wraper usada en classes para compartir el valor de la variable
+     
+    init() {
+        let url = URL(string: "https://jsonplaceholder.typicode.com/posts")! //forzar valor
+        // 2.
+        URLSession.shared.dataTask(with: url) {(data, response, error) in
+            do {
+                if let todoData = data {
+                    // 3.
+                    let decodedData = try JSONDecoder().decode([Todo].self, from: todoData)
+                     //print(decodedData)
+                    DispatchQueue.main.async {
+                        self.todos = decodedData
+                    }
+                } else {
+                    print("No data")
+                }
+            } catch {
+                print("Error")
+            }
+        }.resume()
+    }
+}
